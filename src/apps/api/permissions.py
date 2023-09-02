94b29a253f68ceaa1448cc83e08bff2885afbf60
pyguru-dev <crazybrainy333@skiff.com>
2023-09-02 21:47:02 +0330

    change admin panel font

--- src/apps/api/permissions.py
+++ src/apps/api/permissions.py
@@ -7,3 +7,11 @@ class IsSuperUserOrReadOnly(BasePermission):
         return bool(
             request.method in SAFE_METHODS or request.user and request.user.is_superuser
         )
+
+    
+class IsAuthor(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return bool(
+            request.method in SAFE_METHODS or 
+            request.user and request.user == obj.author.user
+        )
--- src/djshop/envs/common.py
+++ src/djshop/envs/common.py
@@ -136,7 +136,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa-ir'
 
 TIME_ZONE = 'UTC'
 
